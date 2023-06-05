155cd18764aca4a44e6b49782fce3a54f4c59bc3
Kaurvi <kaurvi@mail.uc.edu>
2023-06-04 20:03:50 -0400

    added the code

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static <string> void main(String[] args)
+    {
+       double myLunchCost = 12.50;
+    System.out.println("The cost of my lunch is"+" "+myLunchCost);
+    int numKids = 2;
+        System.out.println("The number of kids in my family is"+" "+numKids);
+        int gasPrice = 4;
+        System.out.println("The price of gallon of gas is"+" "+gasPrice);
+        int favNumber = 30;
+        System.out.println("My fav number is"+" "+favNumber);
+        int shoeSize = 7;
+        System.out.println("My shoe size is"+" "+shoeSize);
+        String raining = "yes";
+        System.out.println("Is it Raining?"+" "+raining);
+        String birthMonth = "March";
+        System.out.println("My birth month is"+" "+birthMonth);
+        String fullName = "Vishavjit Kaur";
+        System.out.println("My Full Name is"+" "+fullName);
+    }
+}
